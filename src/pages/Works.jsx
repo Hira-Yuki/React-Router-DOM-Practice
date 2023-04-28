ee18baab0a81c0bc1bf1300f9641ee71267fa88b
Hira-Yuki <cjsdlf44@gmail.com>
2023-04-29 00:11:19 +0900

    React-Router-DOM 연습

---
+++ src/pages/Works.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { data } from "../shared/data";
+
+
+function Works() {
+  return (
+    <div>
+      <h3>할일 목록</h3>
+      {data.map((cur) => {
+        return (
+          <div key={cur.id}>
+            {cur.id}&nbsp;
+            
+            <Link to={`/works/${cur.id}`}>{cur.todo}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Works;
